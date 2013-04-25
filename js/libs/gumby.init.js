c39d342c80fc971cdb9712280fb63f90970a1ff5
chambaz <adammchambers@googlemail.com>
2013-04-25 12:06:17 +0100

    Comment up gumby.init.js

--- js/libs/gumby.init.js
+++ js/libs/gumby.init.js
@@ -1,13 +1,21 @@
+/**
+* Gumby Init
+*/
+
+// test for touch event support
 Modernizr.load({
 	test: Modernizr.touch,
-	nope: 'js/libs/jquery.mobile.custom.min.js',
+
+	// if present load custom jQuery mobile build and update Gumby.click
+	yep: 'js/libs/jquery.mobile.custom.min.js',
 	callback: function(url, result, key) {
 		if(result) {
 			window.Gumby.click = 'tap';
 		}
 	},
+
+	// either way initialize Gumby
 	complete: function() {
-		// initialize Gumby
 		window.Gumby.init();
 
 		// if AMD return Gumby object to define
