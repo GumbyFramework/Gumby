a22c02d00a451241533e882d3147bea090979207
chambaz <adammchambers@googlemail.com>
2013-05-14 11:40:16 -0400

    Wait for window.load before setting starting position of bg image based on offset().top

--- js/libs/ui/gumby.parallax.js
+++ js/libs/ui/gumby.parallax.js
@@ -8,21 +8,29 @@
 	function Parallax($el) {
 
 		this.$el = $el;
+
 		var scope = this;
 
+		// set starting position of background image
+		this.$el.css('backgroundPosition', '50% '+(this.$el.offset().top * -1)+'px');
 	}
 
 	// add initialisation
 	Gumby.addInitalisation('parallax', function() {
-		$('.parallax').each(function() {
-			var $this = $(this);
-			// this element has already been initialized
-			if($this.data('isParallax')) {
-				return true;
-			}
-			// mark element as initialized
-			$this.data('isParallax', true);
-			new Parallax($this);
+		// wait for window to load as this could effect position of element
+		$(window).load(function() {
+			setTimeout(function() {
+				$('.parallax').each(function() {
+					var $this = $(this);
+					// this element has already been initialized
+					if($this.data('isParallax')) {
+						return true;
+					}
+					// mark element as initialized
+					$this.data('isParallax', true);
+					new Parallax($this);
+				});
+			}, 200);
 		});
 	});
 
