4d7a3b6fe1413d6572918a76bc3c1b52dab9a27b
chambaz <adammchambers@googlemail.com>
2013-05-14 12:06:41 -0400

    Basic parallaxing in place. Element bg position updated at a 1:1 relative to natural scroll

--- js/libs/ui/gumby.parallax.js
+++ js/libs/ui/gumby.parallax.js
@@ -8,12 +8,28 @@
 	function Parallax($el) {
 
 		this.$el = $el;
+		this.$holder = $(window);
+		this.startPos = this.$el.offset().top * -1;
 
 		var scope = this;
 
 		// set starting position of background image
-		this.$el.css('backgroundPosition', '50% '+(this.$el.offset().top * -1)+'px');
+		this.setPosition();
+
+		this.$holder.scroll(function() {
+			scope.scroll();
+		});
 	}
+
+	// update bg position based on scroll and parallax ratio
+	Parallax.prototype.scroll = function() {
+		this.setPosition(this.startPos + this.$holder.scrollTop());
+	};
+
+	// set background y axis position with 50% x axis
+	Parallax.prototype.setPosition = function(yPos) {
+		this.$el.css('backgroundPosition', '50% '+yPos+'px');
+	};
 
 	// add initialisation
 	Gumby.addInitalisation('parallax', function() {
