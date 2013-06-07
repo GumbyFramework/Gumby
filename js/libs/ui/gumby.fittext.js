5bab140c7c523dad2d998585f5bd5b57bb7fd677
chambaz <adammchambers@googlemail.com>
2013-06-07 18:35:27 -0400

    Reminified gumby.min.js and set ui.html back to use the optimised file

--- js/libs/ui/gumby.fittext.js
+++ js/libs/ui/gumby.fittext.js
@@ -10,42 +10,49 @@
 
 	function FitText($el) {
 		this.$el = $el;
-		// compressor rate
+		// optional compressor rate
 		this.rate = Gumby.selectAttr.apply(this.$el, ['rate']) || 1;
-		// parse font sizes (min|max)
+		// optional font sizes (min|max)
 		this.fontSizes = this.parseSizes(Gumby.selectAttr.apply(this.$el, ['sizes']));
 
 		var scope = this;
 
+		// lets go
 		$(window).on('load resize orientationchange', function() {
 			scope.resize();
 		});
 	}
 
+	// apply the resizing
 	FitText.prototype.resize = function() {
 		this.$el.css('font-size', this.calculateSize());
 	};
 
+	// calculate the font size
 	FitText.prototype.calculateSize = function() {
 		return Math.max(Math.min(this.$el.width() / (this.rate*10), parseFloat(this.fontSizes.max)), parseFloat(this.fontSizes.min));
 	};
 
+	// parse size attributes with min|max syntax
 	FitText.prototype.parseSizes = function(attrStr) {
 		var sizes = {
-				min: Number.NEGATIVE_INFINITY,
-				max: Number.POSITIVE_INFINITY
-			};
+			min: Number.NEGATIVE_INFINITY,
+			max: Number.POSITIVE_INFINITY
+		};
 
+		// attribute is optional
 		if(!attrStr) { return sizes; }
 
 		// min and/or max specified
 		if(attrStr.indexOf('|') > -1) {
 			attrStr = attrStr.split('|');
 
+			// both are optional
 			sizes.min = Number(attrStr[0]) || sizes.min;
 			sizes.max = Number(attrStr[1]) || sizes.max;
 		}
 
+		// only one value specific without | so use as min
 		sizes.min = Number(attrStr) || sizes.min;
 
 		return sizes;
