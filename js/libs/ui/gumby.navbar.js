589e2b0eb842a9c81b085ae815aea7ac604308c3
Adam Chambers <adammchambers@gmail.com>
2013-06-30 13:16:59 -0400

    Support for swiperight and tappable icon for opening dropdown links on touch

--- js/libs/ui/gumby.navbar.js
+++ js/libs/ui/gumby.navbar.js
@@ -15,23 +15,55 @@
 		var scope = this;
 
 		// when navbar items are tapped hide/show dropdowns
-		this.$el.find('li').on('tap', function(e) {
+		this.$el.find('li:has(.dropdown)').on('tap', function(e) {
 			// prevent click from triggering here too
 			e.stopImmediatePropagation();
 			e.preventDefault();
 
 			var $this = $(this);
 
-			// we have dropdowns so open/cose
-			if($this.children('.dropdown').length) {
-				if($this.hasClass('active')) {
-					$this.removeClass('active');
-				} else {
-					$this.addClass('active');
-				}
+			if($this.hasClass('active')) {
+				$this.removeClass('active');
+			} else {
+				$this.addClass('active');
 			}
-		});
+
+		// swiping right opens link
+		}).on('swiperight', this.openLink)
+
+		// append open link icon 
+		.children('a').append('<i class="icon-export"></i>').children('i')
+
+		// and bind to click event to open link
+		.on('tap', this.openLink);
 	}
+
+	// handle opening list item link 
+	Navbar.prototype.openLink = function(e) {
+		e.stopImmediatePropagation();
+		e.preventDefault();
+
+		var $this = $(this),
+			$el, href;
+
+		// tapped icon
+		if($this.is('i')) {
+			$el = $this.parent('a');
+		// swiped li
+		} else if($this.is('li')) {
+			$el = $this.children('a');
+		}
+
+		href = $el.attr('href');
+
+		// open in new window
+		if($el.attr('target') == 'blank') {
+			window.open(href);
+		// regular relocation
+		} else {
+			window.location = href;
+		}
+	};
 
 	// add initialisation
 	Gumby.addInitalisation('navbars', function() {
