2131004398db7237040284a3c0c3e1d1793ada98
craigkeller <craig@digitalsurgeons.com>
2013-11-26 16:12:19 -0500

    last commits

--- config.rb
+++ config.rb
@@ -17,13 +17,13 @@ sass_dir = "sass"
 images_dir = "img"
 
 # You can select your preferred output style here (can be overridden via the command line) :nested or :expanded or :compact or :compressed:
-output_style = :expanded
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = true
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
